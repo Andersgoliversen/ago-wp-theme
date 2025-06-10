c8deed7bc7ad5fba40e86bf51ec8efec06a1569a
Anders Gjesdal Oliversen <45917529+Andersgoliversen@users.noreply.github.com>
2025-06-10 19:47:48 +0200

    Improve search result layout

--- search.php
+++ search.php
@@ -7,10 +7,36 @@
     if ( have_posts() ) :
         while ( have_posts() ) :
             the_post();
-            the_title( '<h2 class="text-2xl font-semibold mb-4">', '</h2>' );
-            the_excerpt();
+            ?>
+            <article class="space-y-4">
+                <?php
+                the_title(
+                    '<h2 class="text-2xl font-semibold mb-4"><a href="' . esc_url( get_permalink() ) . '">',
+                    '</a></h2>'
+                );
+
+                if ( has_post_thumbnail() ) {
+                    echo '<a href="' . esc_url( get_permalink() ) . '">';
+                    the_post_thumbnail( 'large', [ 'class' => 'w-full max-h-[48rem] object-cover object-center' ] );
+                    echo '</a>';
+                }
+
+                the_excerpt();
+
+                echo '<time datetime="' . esc_attr( get_the_date( 'c' ) ) . '" class="block text-sm italic text-neutral-500">' . esc_html( get_the_date( 'F j, Y' ) ) . '</time>';
+                ?>
+            </article>
+            <?php
         endwhile;
-        the_posts_pagination();
+        the_posts_pagination(
+            [
+                'mid_size'           => 2,
+                'end_size'           => 1,
+                'prev_text'          => __( '&larr; Previous Page', 'andersgoliversen' ),
+                'next_text'          => __( 'Next Page &rarr;', 'andersgoliversen' ),
+                'screen_reader_text' => '',
+            ]
+        );
     else :
         echo '<p>' . esc_html__( 'No results found.', 'andersgoliversen' ) . '</p>';
     endif;
