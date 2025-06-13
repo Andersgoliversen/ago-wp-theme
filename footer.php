77f4afc81871cd3ab7754417dd71774727212bd0
Anders Gjesdal Oliversen <45917529+Andersgoliversen@users.noreply.github.com>
2025-06-13 12:23:58 +0200

    Use round YouTube icon

--- footer.php
+++ footer.php
@@ -29,8 +29,9 @@
         <a href="https://www.youtube.com/@andersgoliversen" target="_blank" rel="noopener"
            class="no-underline text-inherit decoration-transparent transition-transform duration-150 hover:scale-105 hover:text-neutral-600"
            aria-label="YouTube channel">
-            <svg viewBox="0 0 24 24" class="w-5 h-5 fill-current">
-                <path d="M23.5 6.2a2.99 2.99 0 0 0-2.1-2.1C19.4 3.5 12 3.5 12 3.5s-7.4 0-9.4.6a2.99 2.99 0 0 0-2.1 2.1A31.2 31.2 0 0 0 0 12a31.2 31.2 0 0 0 .5 5.8 2.99 2.99 0 0 0 2.1 2.1c2 .6 9.4.6 9.4.6s7.4 0 9.4-.6a2.99 2.99 0 0 0 2.1-2.1 31.2 31.2 0 0 0 .5-5.8 31.2 31.2 0 0 0-.5-5.8zM9.6 15.5v-7l6.4 3.5-6.4 3.5z"/>
+            <svg viewBox="0 0 48 48" class="w-5 h-5" aria-hidden="true">
+                <circle cx="24" cy="24" r="20" fill="#FF1A1A" class="youtube-bg" />
+                <polygon points="20,32 20,16 32,24" fill="#fff"/>
             </svg>
         </a>
     </nav>
